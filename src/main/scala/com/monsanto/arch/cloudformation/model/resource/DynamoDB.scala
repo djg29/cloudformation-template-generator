470e56e92754e6dc8ae1839ee0934f36648eb2ab
djg29 <gogoidhrub@gmail.com>
2018-09-06 17:39:27 +0200

    Add tagging

--- src/main/scala/com/monsanto/arch/cloudformation/model/resource/DynamoDB.scala
+++ src/main/scala/com/monsanto/arch/cloudformation/model/resource/DynamoDB.scala
@@ -20,6 +20,7 @@ case class `AWS::DynamoDB::Table`(
                                    StreamSpecification : Option[StreamSpecification] = None,
                                    TableName: Option[Token[String]],
                                    TimeToLiveSpecification: Option[TimeToLiveSpecification] = None,
+                                   Tags: Option[Seq[AmazonTag]] = None,
                                    override val Condition: Option[ConditionRef] = None,
                                    override val DeletionPolicy: Option[DeletionPolicy] = None,
                                    override val DependsOn: Option[Seq[String]] = None
@@ -40,7 +41,7 @@ case class `AWS::DynamoDB::Table`(
 }
 
 object `AWS::DynamoDB::Table` {
-  implicit val format: JsonFormat[`AWS::DynamoDB::Table`] = jsonFormat12(`AWS::DynamoDB::Table`.apply)
+  implicit val format: JsonFormat[`AWS::DynamoDB::Table`] = jsonFormat13(`AWS::DynamoDB::Table`.apply)
 }
 
 sealed abstract class StreamViewType(val name : String)
